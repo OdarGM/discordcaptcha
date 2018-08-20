7b104ce9a8525e6540c982c2cbca29df7fffd2b0
Timo <artur.fiedler54@gmail.com>
2018-08-20 19:41:15 +0200

    ES6ify commands

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,12 +1,17 @@
-/**
- * @param {Object} message - The message object, needed to get mentioned users, check if member has ADMINISTRATOR permissions and convert the first mentioned user in a guildmember 
- * @param {array} contrib - Users that are allowed to use that command
- * @returns {promise} message - The "missing permissions" promise (will return if member has not enough permissions
-**/
-module.exports = (message, contrib) => {
-	if(!contrib || message.mentions.users.size === 0) return message.reply("you either aren't allowed to use the command or you did not mention an user.");
-	if(contrib.includes(message.author.tag)) {
-		message.mentions.members.first().ban().catch(console.log);
-		message.reply("banned " + message.mentions.users.first().tag).catch(console.log);
+class Ban {
+	/**
+	* @param {Object} message - The message object, needed to get mentioned users, check if member has ADMINISTRATOR permissions and convert the first mentioned user in a guildmember 
+ 	* @param {array} contrib - Users that are allowed to use that command
+ 	* @returns {promise<void>}
+	**/
+	static async run(message, contrib) {
+		if(!contrib || message.mentions.users.size === 0) return message.reply("you either aren't allowed to use the command or you did not mention an user.");
+		if(contrib.includes(message.author.tag)) {
+			await message.mentions.members.first().ban().catch(console.log);
+			await message.reply("banned " + message.mentions.users.first().tag).catch(console.log);
+		}
 	}
 };
+
+
+module.exports = Ban;
--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,16 +1,20 @@
-/**
- * @param {Object} message - The message object
- * @param {array} contrib - Users that are allowed to use that command
-**/
-module.exports = (message, contrib) => {
-    try {
-        if (contrib.includes(message.author.tag)) {
-            let amount = parseInt(message.content.split(" ")[1]);
-            message.channel.bulkDelete(++amount);
-        } else {
-            return message.channel.send("Missing Permissions");
+class Clear {
+    /**
+    * @param {Object} message - The message object
+    * @param {array} contrib - Users that are allowed to use that command
+    **/
+    static run(message, contrib) {
+        try {
+            if (contrib.includes(message.author.tag)) {
+                let amount = parseInt(message.content.split(" ")[1]);
+                message.channel.bulkDelete(++amount);
+            } else {
+                return message.channel.send("Missing Permissions");
+            }
+        } catch (e) {
+            console.log("[DISCORDCAPTCHA-clearMessages] >> " + e);
         }
-    } catch (e) {
-        console.log("[DISCORDCAPTCHA-clearMessages] >> " + e);
     }
 };
+
+module.exports = Clear;
